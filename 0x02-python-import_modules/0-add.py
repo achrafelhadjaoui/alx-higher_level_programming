21d2f8791e674a3b06b264d55821b1ea1b3d2911
achrafelhadjaoui <aelhadjaoui23@gmail.com>
2024-01-04 18:02:45 -0800

    update first task

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-    import add_0
+    from add_0 import add
 
     a = 1
     b = 2
-    c = add_0.add(a, b)
+    c = add(a, b)
     print(f"{a:d} + {b:d} = {c:d}")
