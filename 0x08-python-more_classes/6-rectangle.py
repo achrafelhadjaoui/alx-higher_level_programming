592d9c4103253fe52ed7dcf1337fc9d38dc07729
achrafelhadjaoui <aelhadjaoui23@gmail.com>
2024-01-31 14:25:24 -0800

    fix the messing e

--- 0x08-python-more_classes/6-rectangle.py
+++ 0x08-python-more_classes/6-rectangle.py
@@ -63,4 +63,4 @@ class Rectangle:
 
     def __del__(self):
         Rectangle.number_of_instances -= 1
-        print("By rectangle...")
+        print("Bye rectangle...")
--- 0x08-python-more_classes/7-rectangle.py
+++ 0x08-python-more_classes/7-rectangle.py
@@ -64,4 +64,4 @@ class Rectangle:
 
     def __del__(self):
         Rectangle.number_of_instances -= 1
-        print("By rectangle...")
+        print("Bye rectangle...")
