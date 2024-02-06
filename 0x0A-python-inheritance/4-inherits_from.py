7177fab1f9aaff0855e7c45e053ba26a5b26fc02
achrafelhadjaoui <aelhadjaoui23@gmail.com>
2024-02-05 18:55:36 -0800

    update

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -1,6 +1,11 @@
 #!/usr/bin/python3
+"""Define inherits_from func"""
+
 
 def inherits_from(obj, a_class):
+    """Method that return True if an object is an instance of a class
+    that inherited from"""
+
     if type(obj) is a_class:
         return False
     else:
