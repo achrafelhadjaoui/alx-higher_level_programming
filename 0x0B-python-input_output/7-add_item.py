eb114e815785932da4ab6e6710a2501f5341e639
achrafelhadjaoui <aelhadjaoui23@gmail.com>
2024-02-07 11:23:45 -0800

    load add save

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
 
+""" a script that adds all arguments to a Python list,
+and then save them to a file"""
+
 import json
 import sys
-
-
 load_function = __import__('6-load_from_json_file').load_from_json_file
 save_function = __import__('5-save_to_json_file').save_to_json_file
 
