0b8440c5767315b408a0dc4bf255be5540b274f4
achrafelhadjaoui <aelhadjaoui23@gmail.com>
2024-02-06 16:46:58 -0800

    update task0

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -9,6 +9,4 @@ def read_file(filename=""):
        Args: filename"""
 
     with open(filename, mode="r", encoding="utf-8") as f:
-        read_data = f.read()
-
-    print(read_data)
+        print(f.read())
