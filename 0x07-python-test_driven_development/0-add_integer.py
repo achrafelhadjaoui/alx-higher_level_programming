f08491379ff7875d43d34238a07221ff73042f64
achrafelhadjaoui <aelhadjaoui23@gmail.com>
2024-02-25 14:56:41 -0800

    update

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,20 +1,11 @@
 #!/usr/bin/python3
-
-"""
-    add integer module
-    this module have one function add_integer()
-
-"""
+"""A function that adds two integers."""
 
 
 def add_integer(a, b=98):
-    """Return the sum of a and b,
-    otherwise raise a TypeError exception with a message
-    """
-
-    if not (isinstance(a, int)) and not (isinstance(a, float)):
+    """A function that return int(a) + int(b)."""
+    if type(a) is not int and type(a) is not float:
         raise TypeError("a must be an integer")
-    elif not isinstance(b, int) and not isinstance(b, float):
+    if type(b) is not int and type(b) is not float:
         raise TypeError("b must be an integer")
-    else:
-        return(int(a) + int(b))
+    return int(a) + int(b)
