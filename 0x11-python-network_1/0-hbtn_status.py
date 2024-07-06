484a981baa99ebe4a8977dd192d63255ce00b339
achrafelhadjaoui <aelhadjaoui23@gmail.com>
2024-07-06 05:00:32 -0700

    update task 0

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,17 +1,14 @@
 #!/usr/bin/python3
-from urllib import request
+"""Script Fetches the url https://alx-intranet.hbtn.io/status
+using the urllib"""
+import urllib.request
+
 
 if __name__ == "__main__":
-    url = "https://alx-intranet.hbtn.io/status"
-    
-    # Create a Request object
-    req = request.Request(url)
-    
-    # Open the URL using the Request object
-    with request.urlopen(req) as response:
+    request = urllib.request.Request("https://alx-intranet.hbtn.io/status")
+    with urllib.request.urlopen(request) as response:
         body = response.read()
         print("Body response:")
         print("\t- type: {}".format(type(body)))
         print("\t- content: {}".format(body))
         print("\t- utf8 content: {}".format(body.decode("utf-8")))
-
