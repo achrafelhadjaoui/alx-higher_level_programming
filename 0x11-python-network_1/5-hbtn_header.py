771384775c1e7da2611d0e715015d82af8897cfb
achrafelhadjaoui <aelhadjaoui23@gmail.com>
2024-07-05 19:44:05 -0700

    update tasks

---
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -0,0 +1,10 @@
+#!/us/bin/python3
+''' a Python script '''
+
+import requests
+import sys
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+    res = requests.get(url)
+    print(res.headers.get("X-Request-Id"))
---
+++ 0x11-python-network_1/6-post_email.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+''' a Python script '''
+
+import requests
+import sys
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+    email = sys.argv[2]
+    params = {"email": email}
+    res = requests.post(url, data=params)
+    print(res.text)
