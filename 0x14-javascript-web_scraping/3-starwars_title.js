490a9c3931b60aa668410a19d9be678e2b2e3022
achrafelhadjaoui <aelhadjaoui23@gmail.com>
2024-07-30 19:20:42 -0700

    update 3

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -1,13 +1,8 @@
 #!/usr/bin/node
 const request = require('request');
+const id = process.argv[2];
 
-if (process.argv.length < 3) {
-  console.log('u need <number>');
-  process.exit(1);
-}
-const number = parseInt(process.argv[2]);
-
-request(`https://swapi-api.alx-tools.com/api/films/${number}`, { json: true }, (err, res, body) => {
-  console.error('error:', err); // Print the error if one occurred
-  console.log(body.title); // Print the HTML for the Google homepage.
+request(`https://swapi-api.alx-tools.com/api/films/${id}`, { json: true }, (err, res, body) => {
+  if (err) console.log(err);
+  console.log(body.title);
 });
